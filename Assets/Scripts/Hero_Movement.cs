ecc7208102b78e66a6661cc3491a2013f0f63719
Zuboow <woobuz@o2.pl>
2020-10-14 16:01:09 +0200

    Added new charset template, attack animation in each direction

--- Assets/Scripts/Hero_Movement.cs
+++ Assets/Scripts/Hero_Movement.cs
@@ -4,43 +4,27 @@ using UnityEngine;
 
 public class Hero_Movement : MonoBehaviour
 {
-    public Animator heroAnimator;
     public float playerSpeed;
     public static bool alive = true;
     new Rigidbody2D rigidbody2D;
 
-    // Start is called before the first frame update
     void OnEnable()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        heroAnimator.SetBool("alive", alive);
-
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(x, y);
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
         if (movement.magnitude > 1)
         {
             movement.Normalize();
         }
 
-
         if (alive)
         {
             rigidbody2D.MovePosition(transform.position + playerSpeed * movement * Time.deltaTime);
-
-            heroAnimator.SetFloat("Horizontal", x);
-            heroAnimator.SetFloat("Vertical", y);
-        } else
-        {
-            heroAnimator.SetFloat("Horizontal", 0);
-            heroAnimator.SetFloat("Vertical", 0);
         }
     }
 }
---
+++ Assets/Scripts/Hero_SpriteManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero_SpriteManager : MonoBehaviour
+{
+    public Animator heroAnimator;
+    public static bool alive = true;
+
+    void OnEnable()
+    {
+
+    }
+
+    void Update()
+    {
+        heroAnimator.SetBool("alive", alive);
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        if (alive)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                heroAnimator.SetBool("attack", true);
+            } else
+            {
+                heroAnimator.SetBool("attack", false);
+            }
+
+            if (x != 0 || y != 0)
+            {
+                heroAnimator.SetBool("Moving", true);
+            }
+            else
+            {
+                heroAnimator.SetBool("Moving", false);
+            }
+
+            heroAnimator.SetFloat("Horizontal", x);
+            heroAnimator.SetFloat("Vertical", y);
+            CheckDirection(x,y);
+        }
+        else
+        {
+            heroAnimator.SetFloat("Horizontal", 0);
+            heroAnimator.SetFloat("Vertical", 0);
+        }
+    }
+
+    void CheckDirection(float x, float y)
+    {
+        switch (x)
+        {
+            case var _ when x < 0:
+                heroAnimator.SetFloat("Direction", 0.25f);
+                break;
+            case var _ when x > 0:
+                heroAnimator.SetFloat("Direction", 1f);
+                break;
+            case var _ when x == 0:
+                if (y < 0)
+                {
+                    heroAnimator.SetFloat("Direction", 0f);
+                }
+                else if (y > 0)
+                {
+                    heroAnimator.SetFloat("Direction", 0.75f);
+                }
+                break;
+        }
+    }
+}
