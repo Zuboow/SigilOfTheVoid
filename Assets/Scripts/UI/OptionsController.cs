e8b3c7de653fbe5454363bb1e550505df94557bc
Zuboow <woobuz@o2.pl>
2021-01-08 16:42:10 +0100

    Added Main menu

---
+++ Assets/Scripts/UI/OptionsController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsController : MonoBehaviour
+{
+    public List<GameObject> optionsObjects;
+    Dictionary<string, GameObject> options = new Dictionary<string, GameObject>();
+    GameObject activeGameObject = null;
+    void Start()
+    {
+        foreach (GameObject o in optionsObjects)
+        {
+            o.SetActive(false);
+            options.Add(o.name, o);
+        }
+
+        activeGameObject = options["Main"];
+        activeGameObject.SetActive(true);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void SetActiveWindow(string name)
+    {
+        activeGameObject.SetActive(false);
+        activeGameObject = options[name];
+        activeGameObject.SetActive(true);
+    }
+}
