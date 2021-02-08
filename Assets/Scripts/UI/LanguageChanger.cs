15a61f505889ac5deac307ac6cd07852b1526486
Zuboow <woobuz@o2.pl>
2021-02-08 19:05:09 +0100

    Menu language translation done

---
+++ Assets/Scripts/UI/LanguageChanger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanguageChanger : MonoBehaviour
+{
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject.FindGameObjectWithTag("MainCamera").GetComponent<SettingsManager>().SetLanguage(name);
+        }
+    }
+}
--- Assets/Scripts/UI/SettingsManager.cs
+++ Assets/Scripts/UI/SettingsManager.cs
@@ -9,13 +9,20 @@ using UnityEngine.UI;
 public class SettingsManager : MonoBehaviour
 {
     GameObject resolutionDropdown;
-    public GameObject brightnessSlider, contrastSlider, fullscreenCheckbox, sensitivitySlider, yAxisInvertedCheckbox, polishLangOption, englishLangOption;
+    public GameObject brightnessSlider, contrastSlider, fullscreenCheckbox, sensitivitySlider, yAxisInvertedCheckbox, polishLangOption, englishLangOption, screenBlocker, attackKeyChanger, eqKeyChanger;
+    public List<GameObject> mainMenuSettingObjects;
     bool fullscreen = true, yAxisInverted = false;
-    float brightness = 0, contrast = 0, sensitivity = 0;
-    string resolution = "1920x1080", language = "polish";
+    float brightness = 0, contrast = 0, sensitivity = 50;
+    string resolution = "1920x1080", language = "english", editedKey = null;
+    //keys
+    public Dictionary<string, KeyCode> keySetup = new Dictionary<string, KeyCode>();
+    Settings settings;
 
     void OnEnable()
     {
+        keySetup.Add("attack", KeyCode.Space);
+        keySetup.Add("eq", KeyCode.I);
+
         if (File.Exists(Application.dataPath + "/Resources/config.txt"))
         {
             File.ReadAllText(Application.dataPath + "/Resources/config.txt");
@@ -33,6 +40,19 @@ public class SettingsManager : MonoBehaviour
             resolution = settings[2];
             string[] selectedOption = settings[2].Split('x');
             Screen.SetResolution(Int32.Parse(selectedOption[0]), Int32.Parse(selectedOption[1]), (settings[3] == "1" ? true : false));
+            foreach (KeyCode vKey in Enum.GetValues(typeof(KeyCode)))
+            {
+                if (vKey.ToString() == settings[7])
+                {
+                    keySetup["attack"] = vKey;
+                    attackKeyChanger.GetComponent<Text>().text = vKey.ToString().ToUpper();
+                }
+                if (vKey.ToString() == settings[8])
+                {
+                    keySetup["eq"] = vKey;
+                    eqKeyChanger.GetComponent<Text>().text = vKey.ToString().ToUpper();
+                }
+            }
 
             switch (settings[6])
             {
@@ -45,9 +65,14 @@ public class SettingsManager : MonoBehaviour
                     englishLangOption.GetComponent<Text>().color = new Color32(255, 255, 255, 255);
                     break;
             }
+            TextAsset jsonData = Resources.Load("JSON/settingsTranslation") as TextAsset;
+            Settings s = JsonUtility.FromJson<Settings>(jsonData.text);
+            foreach (GameObject mmSetting in mainMenuSettingObjects)
+            {
+                mmSetting.GetComponent<Text>().text = s.languages[settings[6] == "polish" ? 1 : 0][mmSetting.name];
+            }
         }
-
-
+        screenBlocker.SetActive(false);
     }
 
     void FixedUpdate()
@@ -64,6 +89,14 @@ public class SettingsManager : MonoBehaviour
                     resolutionDropdown.GetComponent<UnityEngine.UI.Dropdown>().value = x;
                 }
             }
+        }
+    }
+
+    private void Update()
+    {
+        if (screenBlocker.activeInHierarchy)
+        {
+            ChangeKey(editedKey);
         }
     }
 
@@ -112,6 +145,67 @@ public class SettingsManager : MonoBehaviour
     public void SetLanguage(string lang)
     {
         language = lang;
+        switch (lang)
+        {
+            case "english":
+                englishLangOption.GetComponent<Text>().color = new Color32(108, 108, 108, 255);
+                polishLangOption.GetComponent<Text>().color = new Color32(255, 255, 255, 255);
+                break;
+            case "polish":
+                polishLangOption.GetComponent<Text>().color = new Color32(108, 108, 108, 255);
+                englishLangOption.GetComponent<Text>().color = new Color32(255, 255, 255, 255);
+                break;
+        }
+        TextAsset jsonData = Resources.Load("JSON/settingsTranslation") as TextAsset;
+        Settings s = JsonUtility.FromJson<Settings>(jsonData.text);
+        foreach (GameObject mmSetting in mainMenuSettingObjects)
+        {
+            mmSetting.GetComponent<Text>().text = s.languages[lang == "polish" ? 1 : 0][mmSetting.name];
+        }
+        
+        SaveConfigFile();
+    }
+
+    public void EditKey(string function)
+    {
+        editedKey = function;
+        screenBlocker.SetActive(true);
+    }
+
+    public void ChangeKey(string function)
+    {
+        foreach (KeyCode vKey in Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                screenBlocker.SetActive(false);
+                editedKey = null;
+            }
+            else if (Input.GetKeyDown(vKey) && !vKey.ToString().Contains("Mouse") && vKey != KeyCode.Percent)
+            {
+                switch (function)
+                {
+                    case "attack":
+                        attackKeyChanger.GetComponent<Text>().text = vKey.ToString().ToUpper();
+                        break;
+                    case "eq":
+                        eqKeyChanger.GetComponent<Text>().text = vKey.ToString().ToUpper();
+                        break;
+                }
+                keySetup[function] = vKey;
+                screenBlocker.SetActive(false);
+                editedKey = null;
+                SaveConfigFile();
+            }
+        }
+    }
+
+    public void SetDefaultKeys()
+    {
+        attackKeyChanger.GetComponent<Text>().text = KeyCode.Space.ToString().ToUpper();
+        eqKeyChanger.GetComponent<Text>().text = KeyCode.I.ToString().ToUpper();
+        keySetup["attack"] = KeyCode.Space;
+        keySetup["eq"] = KeyCode.I;
         SaveConfigFile();
     }
 
@@ -119,7 +213,8 @@ public class SettingsManager : MonoBehaviour
     {
         File.WriteAllText(Application.dataPath + "/Resources/config.txt", String.Empty);
         TextWriter tw = new StreamWriter(Application.dataPath + "/Resources/config.txt", true);
-        tw.WriteLine(brightness + "%" + contrast + "%" + resolution + "%" + (fullscreen ? 1 : 0) + "%" + sensitivity + "%" + (yAxisInverted ? 1 : 0) + "%" + language);
+        tw.WriteLine(brightness + "%" + contrast + "%" + resolution + "%" + (fullscreen ? 1 : 0) + "%" + sensitivity + "%" + (yAxisInverted ? 1 : 0) + "%" + language + "%" 
+            + keySetup["attack"].ToString() + "%" + keySetup["eq"].ToString());
         tw.Close();
     }
 }
